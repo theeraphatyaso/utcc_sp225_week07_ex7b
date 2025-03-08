fb255b9539e132ef90bdf06ca474c34115009f40
theeraphaty <teerapatyaso@gmail.com>
2025-03-08 17:09:26 +0700

    Bug fix#2 - adding student id validation (id must be composed of numbers between 0 and 9)

--- validate.js
+++ validate.js
@@ -1,6 +1,22 @@
+// ADD REGULAR EXPRESSION FUNCTION TO VALIDATE NUMBER ONLY IN STRING ATTRIBUTE
+function isNumeric(value) {
+	/*
+		/^ 			is start position of String
+		?			is optional case
+		/d			is integer
+		+			is the variable type can have more than one char ex: \d+ means can have number more that one number in String
+		$/			is end of the String (in C will be '\0')
+		.test(value)	is get the Regular expression test with the value in its parameter
+		ธีรภััทร์ ยะโส 2310511101047 
+	*/
+
+    return /^\d+$/.test(value);
+}
+
 function checkSID() {
   let sid = (document.getElementById("sid").value).trim();
-  if (sid.length == 10) {
+
+  if (sid.length == 10 && isNumeric(sid)) {
     return true;
   } else {
     return false;
